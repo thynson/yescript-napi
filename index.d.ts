cf9ecee1b2c2dcfd39ffac75032a5681ef5bbc37
LAN Xingcan <thynson@live.com>
2021-08-31 20:24:12 +0800

    chore: clean up comments

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,3 @@
 export declare const YESCRYPT_RW = 2;
 export declare const YESCRYPT_WORM = 1;
 export declare function yescrypt(password: string | Buffer, salt: string | Buffer, N: number, r: number, p: number, t: number, g: number, dkLen: number, flags?: number): Promise<Buffer>;
-//# sourceMappingURL=index.d.ts.map
